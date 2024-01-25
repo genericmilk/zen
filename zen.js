1fc4b1fce8c9f55e885e9e30d42fc386138c7ebb
Peter Day <peterday.main@gmail.com>
2024-01-25 10:43:01 +0000

    Update zen.js

--- zen.js
+++ zen.js
@@ -9,6 +9,8 @@ var zen = {
         'brightPurple'
     ],
     boot:function(){
+        // .glow{height:100%;width:100%;position:absolute;background-color:#000;top:0;left:0;overflow:hidden;pointer-events:none;z-index:-1}.glow .ember{opacity:0;transition:opacity 4s;height:500px;width:500px;position:absolute}.glow .ember.show{opacity:1}.glow .ember.yellow{background:radial-gradient(circle,#5d5026 0%,rgba(93,80,38,0) 50%)}.glow .ember.purple{background:radial-gradient(circle,#3b1766 0%,rgba(59,23,102,0) 50%)}.glow .ember.blue{background:radial-gradient(circle,#2accff 0%,rgba(42,204,255,0) 50%)}.glow .ember.brightPurple{background:radial-gradient(circle,#6f06c1 0%,rgba(111,6,193,0) 50%)}
+        
         if($('.glow').length<1){
             return; // do not spawn if no container
         }
